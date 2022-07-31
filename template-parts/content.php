bc9c0ec939d0e30f72fdd65d707f2b0cea3d975d
monzuralam <monzur707@gmail.com>
2022-07-31 09:27:55 +0600

    displaying post style fix

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,55 +9,61 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
+<div class="col-lg-6">
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<!-- Single blog post item start here  -->
+		<div class="blog-post">
+			<?php if( has_post_thumbnail() ): ?>
+				<div class="blog-thumb">
+					<?php standpress_post_thumbnail(); ?>
+				</div>
+			<?php endif; ?>
+			<div class="down-content">
 				<?php
-				standpress_posted_on();
-				standpress_posted_by();
+					if( has_category() ){
+						foreach((get_the_category()) as $category){
+							echo '<span>'. $category->name."</span> ";
+						}
+					}
 				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+				<?php
+					the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+				?>
+				<ul class="post-info">
+					<li><a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta('ID') ) ); ?>"><?php the_author(); ?></a></li>
+					<li><?php echo esc_html(date('M d, Y')); ?></li>
+					<li><a href="<?php the_permalink(); ?>"><?php echo esc_html__( get_comments_number() . ' Comments', 'standpress' ); ?> </a></li>
+				</ul>
+				<?php 
+					the_excerpt();
 
-	<?php standpress_post_thumbnail(); ?>
-
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'standpress' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'standpress' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php standpress_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+					if( has_tag() ):
+				?>
+				<div class="post-options">
+					<div class="row">
+						<div class="col-lg-12">
+							<ul class="post-tags">
+								<li><i class="fa fa-tags"></i></li>
+								<?php 
+									$post_tags = get_the_tags();
+									$separator = ' , ';
+									$output = '';
+								
+									if ( ! empty( $post_tags ) ) {
+										foreach ( $post_tags as $tag ) {
+											$output .= '<li><a href="' . esc_attr( get_tag_link( $tag->term_id ) ) . '">' . $tag->name . '</a></li>' . $separator;
+										}
+									}
+								
+									echo trim( $output, $separator );
+								?>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<?php endif; ?>
+			</div>
+		</div>
+		<!-- Single blog post item Stop here -->
+	</article><!-- #post-<?php the_ID(); ?> -->
+</div> 
