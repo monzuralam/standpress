304f41c0165d35000ffe2b5866a1902bbf2cdb17
monzuralam <monzur707@gmail.com>
2022-08-01 07:02:53 +0600

    page dynamic

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying page content in page.php
  *
@@ -10,45 +11,66 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+	<!-- Banner Starts Here -->
+	<div class="header-text">
+		<section class="page-heading">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="text-content text-center">
+							<h1><?php the_title(); ?></h1>
+							<span class="text-white"><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?> </a> <?php _e( ' / ', 'standpress' ); esc_html( the_title() ); ?> </span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+	<!-- Banner Ends Here -->
 
-	<?php standpress_post_thumbnail(); ?>
+	<section class="page-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<?php standpress_post_thumbnail(); ?>
+				</div>
+				<div class="col-lg-12">
+					<div class="entry-content">
+						<?php 
+							the_content();
 
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'standpress' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<?php if ( get_edit_post_link() ) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'standpress' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					wp_kses_post( get_the_title() )
-				),
-				'<span class="edit-link">',
-				'</span>'
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
+							wp_link_pages(
+								array(
+									'before' => '<div class="page-links">' . esc_html__('Pages:', 'standpress'),
+									'after'  => '</div>',
+								)
+							);
+						?>
+					</div>
+				</div>
+				<div class="col-md-12">
+					<?php 
+						if (get_edit_post_link()) : 
+							edit_post_link(
+								sprintf(
+									wp_kses(
+										/* translators: %s: Name of current post. Only visible to screen readers */
+										__('Edit <span class="screen-reader-text">%s</span>', 'standpress'),
+										array(
+											'span' => array(
+												'class' => array(),
+											),
+										)
+									),
+									wp_kses_post(get_the_title())
+								),
+								'<span class="edit-link">',
+								'</span>'
+							);
+						endif; 
+					?>
+				</div>
+			</div>
+		</div>
+	</section>
 </article><!-- #post-<?php the_ID(); ?> -->
