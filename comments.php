7445badb58226ff085083616328908966d0e03ea
monzuralam <monzur707@gmail.com>
2022-08-01 21:45:50 +0600

    comment form customize

--- comments.php
+++ comments.php
@@ -26,38 +26,42 @@ if ( post_password_required() ) {
 	// You can start editing here -- including this comment!
 	if ( have_comments() ) :
 		?>
-		<h2 class="comments-title">
-			<?php
-			$standpress_comment_count = get_comments_number();
-			if ( '1' === $standpress_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'standpress' ),
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			} else {
-				printf( 
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $standpress_comment_count, 'comments title', 'standpress' ) ),
-					number_format_i18n( $standpress_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
+		<div class="sidebar-heading">
+			<h2 class="comments-title">
+				<?php
+				$standpress_comment_count = get_comments_number();
+				if ( '1' === $standpress_comment_count ) {
+					printf(
+						/* translators: 1: title. */
+						esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'standpress' ),
+						'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+					);
+				} else {
+					printf( 
+						/* translators: 1: comment count number, 2: title. */
+						esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $standpress_comment_count, 'comments title', 'standpress' ) ),
+						number_format_i18n( $standpress_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+						'<span>' . wp_kses_post( get_the_title() ) . '</span>'
+					);
+				}
+				?>
+			</h2><!-- .comments-title -->
+		</div><!-- .sidebar-heading -->
 
 		<?php the_comments_navigation(); ?>
-
-		<ol class="comment-list">
-			<?php
-			wp_list_comments(
-				array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				)
-			);
-			?>
-		</ol><!-- .comment-list -->
+		
+		<div class="content">
+			<ol class="comment-list">
+				<?php
+				wp_list_comments(
+					array(
+						'style'      => 'ol',
+						'short_ping' => true,
+					)
+				);
+				?>
+			</ol><!-- .comment-list -->
+		</div><!-- .content -->
 
 		<?php
 		the_comments_navigation();
