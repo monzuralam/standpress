0e96174c66440f69192a881ef1186ac39195546a
monzuralam <monzur707@gmail.com>
2022-08-15 09:23:35 +0600

    404 page fix

--- 404.php
+++ 404.php
@@ -11,48 +11,39 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
+		
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<!-- Banner Starts Here -->
+			<div class="header-text">
+				<section class="page-heading">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-12">
+								<div class="text-content text-center">
+									<h1 class=""><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'standpress' ); ?></h1>
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
+			</div>
+			<!-- Banner Ends Here -->
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'standpress' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'standpress' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'standpress' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$standpress_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'standpress' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$standpress_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+			<section class="page-section">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-12">
+							<div class="entry-content text-center">
+								<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'standpress' ); ?></p>
+								<?php 
+									get_search_form();
+								?>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</article><!-- #post-<?php the_ID(); ?> -->
 
 	</main><!-- #main -->
 
