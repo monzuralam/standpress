63119fe6bdaec6d3438d5e1b427dcfbe2658586b
monzuralam <monzur707@gmail.com>
2022-08-20 16:07:40 +0600

    call to action section remove

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -2,31 +2,6 @@
 
 <?php get_template_part( 'template-parts/content', 'banner' ); ?>
 
-<!-- Call to action start here -->
-<section class="call-to-action">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="main-content">
-                    <div class="row">
-                        <div class="col-lg-8">
-                            <span>Stand Blog HTML5 Template</span>
-                            <h4>Creative HTML Template For Bloggers!</h4>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="main-button">
-                                <a rel="nofollow" href="#" target="_parent"><?php esc_html_e('Learn More', 'standpress'); ?></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- Call to action stop here -->
-
-
 <section class="blog-posts grid-system">
     <div class="container">
         <div class="row">
