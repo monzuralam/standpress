fed0b65d212bc1abbfb9e8200ec8a2046aa253de
monzuralam <monzur707@gmail.com>
2022-08-20 23:14:29 +0600

    theme color field register

--- inc/theme-options.php
+++ inc/theme-options.php
@@ -5,6 +5,10 @@ use Carbon_Fields\Field;
 function standpress_theme_options() {
     $standpress_options = Container::make( 'theme_options', __( 'Theme Options', 'standpress' ) )
     ->add_fields( array(
+        Field::make( 'color', 'standpress_primary_color', __( 'Theme Primary Color: ' ) )
+    ) )
+    
+    ->add_fields( array(
         Field::make( 'image', 'standpress_default_banner', __( 'Header default banner' ) )
         ->set_value_type( 'url' )
     ) );
