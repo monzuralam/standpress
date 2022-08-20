0e356e43397380c4c0447fccc41863af4adadbc2
monzuralam <monzur707@gmail.com>
2022-08-20 16:00:05 +0600

    header banner img field for theme option

---
+++ inc/theme-options.php
@@ -0,0 +1,12 @@
+<?php
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+function standpress_theme_options() {
+    $standpress_options = Container::make( 'theme_options', __( 'Theme Options', 'standpress' ) )
+    ->add_fields( array(
+        Field::make( 'image', 'standpress_default_banner', __( 'Header default banner' ) )
+        ->set_value_type( 'url' )
+    ) );
+}
+add_action( 'carbon_fields_register_fields', 'standpress_theme_options' );
